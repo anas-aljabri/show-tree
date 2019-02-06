93be25bd9828c7beb89e1cf9c6005018bab1979b
Anas Aljabri <32190974+anas-aljabri@users.noreply.github.com>
2019-02-06 17:05:02 +0000

    Delete node.ts

--- src/node.ts
+++
@@ -1,9 +0,0 @@
-export class Node {
-    Id: number;
-    Name: string;
-    Children?: Node[];
-    Code?: string;
-    KeywordMatch?: boolean = false;
-    IsOpen?: boolean = false;
-    Fields?: any[];
-}
